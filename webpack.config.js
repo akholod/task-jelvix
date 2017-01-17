d80a5b6cf0bda023d1c5faecb7e1e53d97679c65
akholod <andrey.kokhanevich@gmail.com>
2017-01-17 14:27:44 +0200

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,31 @@
 'use strict';
 
+const path = require('path');
+const webpack = require("webpack");
+
+module.exports = {
+    context: __dirname,
+    entry: './client/app',
+    output: {
+        path: path.join(__dirname, '/client/build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {test: /\.html$/, loader: 'raw-loader'},
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader?sourceMap']
+            }
+        ]},
+    devtool: "source-map",
+    watch: true
+};
